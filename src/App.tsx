2077157538ab9a71c15ae8382196f1e3ffd984ce
Dmitry.Lavrov <Dmitry.Lavrov@x5.ru>
2021-12-07 14:34:51 +0300

    Добавил цели

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import './App.css'
 import data from './data'
 import { cardType, orderType } from './types';
 
+const hashCode = (s: string) => s.split('').reduce((a,b)=>{a=((a<<5)-a)+b.charCodeAt(0);return a&a},0)
+
 function App() {
 
   const [answerInput, setAnswerInput]           = useState('');
@@ -19,7 +21,7 @@ function App() {
   }
 
   const answerCheckHandler = (card: cardType, index: number) => {
-    let answer = answerInput.replace(/[,\.;]/, ',').toLowerCase().trim();
+    let answer = answerInput.replace(/[,\.;]/g, ',').toLowerCase().trim();
     let rightAnswer = card.en.replace(/[,\.;]/g, ',').toLowerCase().trim()
     if (answer === rightAnswer) {
       setCurrentCardIndex(p => p + 1);
@@ -37,74 +39,87 @@ function App() {
     setAnswerText(answer);
   }
 
+	const resetState = () => {
+		setAnswerText('');
+		setWrongTryCout(0);
+		setCurrentCardIndex(0);
+	}
+
 	const setDefaultOrder = () => {
 		setOrder('default');
 		setCardToDraw(data);
+		resetState();
 	}
 
 	const setRandomOrder = ()	=> {
 		setOrder('random');
 		setCardToDraw(shuffleArray(data, {copy: true}));
+		resetState();
 	}
 
 	const setReverseOrder = () => {
 		setOrder('reverse');
 		setCardToDraw([...data].reverse())
+		resetState();
 	}
+
+	let title = 'asd'
 
 
   return (
 		<div className="wrapper">
-			<h2>Порядок</h2>
-			<div className="buttons">
-				<ul>
-					<li>
-						<label onClick={setDefaultOrder}>
-							<span>По порядку</span>
-							<input checked={order === 'default'} name='order' type="radio" />
+			<div className="center">
+				<div className="buttons">
+				<h2 className='buttonsTitle'>{title}</h2>
+					<ul className='buttonsList'>
+						<li className='buttonsItem'>
+							<label onClick={setDefaultOrder}>
+								<input checked={order === 'default'} name='order' type="radio" />
+								<span>По порядку</span>
 							</label>
 						</li>
-					<li>
-						<label onClick={setRandomOrder}>
-							<span>Вразнобой</span>
-							<input checked={order === 'random'} name='order' type="radio" />
+						<li className='buttonsItem'>
+							<label onClick={setRandomOrder}>
+								<input checked={order === 'random'} name='order' type="radio" />
+								<span>Вразнобой</span>
 							</label>
 						</li>
-					<li>
-						<label onClick={setReverseOrder}>
-							<span>Задом наперёд</span>
-							<input checked={order === 'reverse'} name='order' type="radio" />
+						<li className='buttonsItem'>
+							<label onClick={setReverseOrder}>
+								<input checked={order === 'reverse'} name='order' type="radio" />
+								<span>Задом наперёд</span>
 							</label>
 						</li>
-				</ul>
-			</div>
-			<div className='cards'>
-				<div className='card prev'></div>
-				{cardToDraw.map((card, index) => {
-					if (Math.abs(index - currentCardIndex) > 1) return
-					return (
-						<div key={index} className={cn('card ', {
-							prev: index < currentCardIndex,
-							current: index === currentCardIndex,
-							next: index > currentCardIndex,
-						})}>
-							<h2>{card.rus}</h2>
-							<form onSubmit={(e) => {
-								e.preventDefault();
-								answerCheckHandler(card, index);
-							}}>
-								<input className={`input-${index}`} value={answerInput} onChange={changeAnswerHandler}/>
-								<button type='submit'>Проверить</button>
-								{
-									wrongTryCout >= 3 && <button onClick={() => showAnserHandler(card.en)}>Показать ответ</button>
-								}
-								{
-									answerText && <p>{answerText}</p>
-								}
-							</form>
-						</div>
-					)
-				})}
+					</ul>
+				</div>
+				<div className='cards'>
+					<div className='card prev'></div>
+					{cardToDraw.map((card, index) => {
+						if (Math.abs(index - currentCardIndex) > 1) return
+						return (
+							<div key={hashCode(card.rus + card.en)} className={cn('card ', {
+								prev: index < currentCardIndex,
+								current: index === currentCardIndex,
+								next: index > currentCardIndex,
+							})}>
+								<form className='cardForm' onSubmit={(e) => {
+									e.preventDefault();
+									answerCheckHandler(card, index);
+								}}>
+									<h2 className='cardTitle'>{card.rus}</h2>
+									<input className={`input-${index} inputAnswer`} value={answerInput} onChange={changeAnswerHandler}/>
+									<button className='defaultButton checkButton' type='submit'>Проверить</button>
+									{
+										wrongTryCout >= 1 && <button className='defaultButton' onClick={() => showAnserHandler(card.en)}>Показать ответ</button>
+									}
+									{
+										answerText && <p className='answerText'>{answerText}</p>
+									}
+								</form>
+							</div>
+						)
+					})}
+				</div>
 			</div>
 		</div>
   )
